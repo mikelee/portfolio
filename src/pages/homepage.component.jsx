aa33853a4ddfcdad69efed09b175703d71934964
Mike <mike.lee816@gmail.com>
2019-10-15 19:51:29 -0400

    Create skills section

--- src/pages/homepage.component.jsx
+++ src/pages/homepage.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Hero from '../components/hero/hero.component';
+import SkillsSection from '../components/skills-section/skills-section.component';
 import ProjectsSection from '../components/projects-section/projects-section.component';
 import ContactSection from '../components/contact-section/contact-section.component';
 
@@ -9,6 +10,7 @@ import './homepage.styles.scss';
 const HomePage = () => (
     <div className='home-page'>
         <Hero />
+        <SkillsSection />
         <ProjectsSection />
         <ContactSection />
     </div>
