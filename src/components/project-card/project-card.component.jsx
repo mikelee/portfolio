54d1242ca38edb799fc85f087ea3bc49941b796c
Mike <mike.lee816@gmail.com>
2019-10-23 14:23:29 -0400

    Make code more readable

--- src/components/project-card/project-card.component.jsx
+++ src/components/project-card/project-card.component.jsx
@@ -8,7 +8,10 @@ const ProjectCard = ({ name, img, tech, description, index }) => (
             <div className='project-info'>
                 <h3 className='project-title'>{name}</h3>
                 <p className='project-description'>{`${name} ${description}`}</p>
-                <p className='project-tech'>Tech used: {tech.join(', ')}</p>
+                <p className='project-tech'>
+                    <span>Tech used: </span>
+                    {tech.join(', ')}
+                </p>
             </div>
             <img className='project-img'  alt='project' src={img} />
         </div>
