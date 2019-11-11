f4dad88c25565d9bfade5c2e94d28ac3a330d883
Mike <mike.lee816@gmail.com>
2019-11-10 20:57:43 -0500

    Change statement and submit button text

--- src/components/contact-section/contact-section.component.jsx
+++ src/components/contact-section/contact-section.component.jsx
@@ -5,13 +5,13 @@ import './contact-section.styles.scss';
 const Contact = () => (
     <div className='contact-section'>
         <h3 className='title'>Contact Me</h3>
-        <p className='statement'>Currently looking to join an exciting company!</p>
+        <p className='statement'>I'm currently looking to join an exciting company!</p>
         <form action='https://usebasin.com/f/e94fc554851b' method='POST'>
             <input type='email' name='email' placeholder='Email address' required />
             <textarea name='textarea' rows='12' placeholder="Let's talk..." required></textarea>
             <div className='form-buttons'>
                 <div className="g-recaptcha" data-sitekey="6Lew3SMUAAAAAJ82QoS7gqOTkRI_dhYrFy1f7Sqy" data-size="compact" data-theme="dark"></div>
-                <button type='submit'>Submit</button>
+                <button type='submit'>SUBMIT</button>
             </div>
         </form>
         <a className='attribution' href="https://www.freepik.com/free-photos-vectors/background">Background vector created by starline - www.freepik.com</a>
