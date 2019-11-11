0278ff1c2e9cf30910a80868e5596cdbd386be99
Mike <mike.lee816@gmail.com>
2019-11-10 21:00:41 -0500

    Fix logic to determine when alternate class is applied

--- src/components/project-card/project-card.component.jsx
+++ src/components/project-card/project-card.component.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import './project-card.styles.scss';
 
 const ProjectCard = ({ name, img, tech, description, index }) => (
-    <div className={`project-card ${index % 2 !== 0 ? "alternate" : ""}`} >
+    <div className={`project-card ${index !== 0 ? "alternate" : ""}`} >
         <div className='content'>
             <div className='project-info'>
                 <h3 className='project-title'>{name}</h3>
-                <p className='project-description'>{`${name} ${description}`}</p>
+                <p className='project-description'>{description}</p>
                 <p className='project-tech'>
                     <span>Tech used: </span>
                     {tech.join(', ')}
