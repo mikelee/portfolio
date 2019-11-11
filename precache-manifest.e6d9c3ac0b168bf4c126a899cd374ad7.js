fd456350968bac66f39e1d609fd2eedae930703f
Mike <mike.lee816@gmail.com>
2019-11-11 10:21:30 -0500

    Updates

---
+++ precache-manifest.e6d9c3ac0b168bf4c126a899cd374ad7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f621a6091f5d7df80ee4ad80bf955ca3",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c26dc6927aa9161230ec",
+    "url": "/portfolio/static/css/main.b76bfbfb.chunk.css"
+  },
+  {
+    "revision": "9fe3ab29d92efe9ac6fe",
+    "url": "/portfolio/static/js/2.5b1a06f7.chunk.js"
+  },
+  {
+    "revision": "c26dc6927aa9161230ec",
+    "url": "/portfolio/static/js/main.4ffc22c7.chunk.js"
+  },
+  {
+    "revision": "791f997dc0acbb20fb93",
+    "url": "/portfolio/static/js/runtime-main.021c856b.js"
+  },
+  {
+    "revision": "2deabba42912d3c4fe6e74cb8b3fb983",
+    "url": "/portfolio/static/media/20804.2deabba4.jpg"
+  },
+  {
+    "revision": "fcc32811d6f4dc63692d6619f2a1d364",
+    "url": "/portfolio/static/media/CornDemo-Regular.fcc32811.ttf"
+  },
+  {
+    "revision": "62a8425826d8c50b5b4784779e20114b",
+    "url": "/portfolio/static/media/CovesLight.62a84258.otf"
+  },
+  {
+    "revision": "91432d4eb75c26536eb7976e12ddd379",
+    "url": "/portfolio/static/media/FadedCode.91432d4e.png"
+  },
+  {
+    "revision": "064ace4777ac86d7627f455f36975452",
+    "url": "/portfolio/static/media/GitHub_Logo_White.064ace47.png"
+  },
+  {
+    "revision": "393e4cd59d7f3042b062f0c4386b9fa3",
+    "url": "/portfolio/static/media/crown-image.393e4cd5.png"
+  },
+  {
+    "revision": "e51381bcb4297ff6659dba96a7f922f2",
+    "url": "/portfolio/static/media/me_pic.e51381bc.png"
+  },
+  {
+    "revision": "d3bcc15cdf24ad2bc5e23a14bc6c8124",
+    "url": "/portfolio/static/media/mindright-image.d3bcc15c.png"
+  },
+  {
+    "revision": "8036e4c9a32b120c9bb6bbadbd57bec3",
+    "url": "/portfolio/static/media/portfolio-image.8036e4c9.png"
+  }
+]);
