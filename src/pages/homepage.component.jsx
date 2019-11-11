90cac47f51c483724f68726712fcd7255beb9f31
Mike <mike.lee816@gmail.com>
2019-11-10 19:37:06 -0500

    Add about section

--- src/pages/homepage.component.jsx
+++ src/pages/homepage.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Hero from '../components/hero/hero.component';
+import AboutSection from '../components/about-section/about-section.component'
 import SkillsSection from '../components/skills-section/skills-section.component';
 import ProjectsSection from '../components/projects-section/projects-section.component';
 import ContactSection from '../components/contact-section/contact-section.component';
@@ -10,6 +11,7 @@ import './homepage.styles.scss';
 const HomePage = () => (
     <div className='home-page'>
         <Hero />
+        <AboutSection />
         <SkillsSection />
         <ProjectsSection />
         <ContactSection />
