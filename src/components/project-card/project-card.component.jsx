5dbdaad761f76e27b83073524989830323067155
Mike <mike.lee816@gmail.com>
2020-04-24 15:33:53 -0400

    Decrease image size and make other sizing tweaks. Move project title outside of content div

--- src/components/project-card/project-card.component.jsx
+++ src/components/project-card/project-card.component.jsx
@@ -4,9 +4,9 @@ import './project-card.styles.scss';
 
 const ProjectCard = ({ name, img, tech, description, index }) => (
     <div className={`project-card ${index !== 0 ? "alternate" : ""}`} >
+        <h3 className='project-title'>{name}</h3>
         <div className='content'>
             <div className='project-info'>
-                <h3 className='project-title'>{name}</h3>
                 <p className='project-description'>{description}</p>
                 <p className='project-tech'>
                     <span>Tech used: </span>
