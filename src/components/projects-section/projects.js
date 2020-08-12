82eae3f6607922ef99c626c434ac774e99cbe92f
Mike <mike.lee816@gmail.com>
2020-08-12 19:47:53 -0400

    Change project tech from array to string

--- src/components/projects-section/projects.js
+++ src/components/projects-section/projects.js
@@ -8,7 +8,7 @@ const projects = [
         name: 'Daily Achievements',
         img: dailyAchievementsImage,
         description: 'A to do list app with a twist. To dos are linked to rewards. When all the required to dos are completed, the reward is unlocked.',
-        tech: ['Javascript, React, Redux, Node.js, Express, MySQL, Passport.js'],
+        tech: 'Javascript, React, Redux, Node.js, Express, MySQL, Passport.js',
         url: 'https://dailyachievements.herokuapp.com'
     },
     {
@@ -16,7 +16,7 @@ const projects = [
         name: 'Mind Right',
         img: mindRightImage,
         description: 'Keep a postive mindset and stay focused on what matters with MindRight. Add quotes with a background image. Simply click shuffle when you need a reminder and one of your quotes will appear.',
-        tech: ['Javascript, EJS, Node.js, Express, MongoDB, Mongoose, Passport.js'],
+        tech: 'Javascript, EJS, Node.js, Express, MongoDB, Mongoose, Passport.js',
         url: 'https://mind-right.herokuapp.com'
     },
     {
@@ -24,7 +24,7 @@ const projects = [
         name: 'Crown Clothing',
         img: crownClothingImage,
         description: 'Browse through clothing collections, add items to your cart, and checkout on this e-commerce clothing website.',
-        tech: ['Javascript', 'React', 'Redux', 'Firebase', 'Stripe'],
+        tech: 'Javascript, React, Redux, Firebase, Stripe',
         url: 'http://crown-clothing-ml.herokuapp.com'
     }
 ];
